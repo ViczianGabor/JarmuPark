315973de4de156d35491cb230d3a7aebb9e565e7
ViczianGabor <viczian.gabor13@gmail.com>
2020-09-21 13:04:01 +0200

    működő auto osztály

---
+++ JarmuPark/Auto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarmuPark
+{
+    class Auto
+    {
+        private string szin;
+        private string tipus;
+        private double uzemanyag;
+        private double fogyaszt;
+        private double km;
+        private double megteheto;
+
+
+
+        public Auto (string szin, string tipus, double fogyaszt)
+        {
+            this.szin = szin;
+            this.tipus = tipus;
+            this.fogyaszt = fogyaszt;
+            this.km = 0;
+            this.uzemanyag = 0;
+        }
+
+        public void tankol(double mennyit)
+        {
+
+            uzemanyag += mennyit;
+            megtehetoo();
+
+        }
+
+        public void megy (double km)
+        {
+            this.km += km;
+            uzemanyag -= (km*fogyaszt/100);
+            megtehetoo();
+
+
+        }
+
+        public void allapot()
+        {
+
+            Console.WriteLine($"{ tipus} - { szin}");
+            Console.WriteLine($"KM óra {km}");
+            Console.WriteLine($"Üzemanyag: {uzemanyag} l");
+            Console.WriteLine($"fogyasztás: {fogyaszt} l/100km");
+            Console.WriteLine("Megtehető km: {0}",megteheto);
+
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private void megtehetoo() 
+        {
+            megteheto = uzemanyag / fogyaszt * 100;
+
+        }
+
+    }
+}
--- JarmuPark/Program.cs
+++ JarmuPark/Program.cs
@@ -10,12 +10,37 @@ namespace JarmuPark
     {
         static void Main(string[] args)
         {
+            
+            Auto Lada = new Auto("Piros","Lada 2112",11);
+            Auto Trabant = new Auto("Zöld","Trabant 1.1",8);
+
+
+            Lada.tankol(20);
+            Trabant.tankol(20);
+
+
+            Lada.allapot();
+            Trabant.allapot();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Mennyit menjen a Lada?");
+            int ee = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mennyit menjen a Trabant?");
+            int aa = int.Parse(Console.ReadLine());
+            
+            Lada.megy(ee);
+            Trabant.megy(aa);
+
+
+
+            Lada.allapot();
+            Trabant.allapot();
 
 
 
 
-
-
+            Console.ReadKey();
         }
     }
 }
